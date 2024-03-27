bfc06b6749329b5d9d477800e319cac8485b73b5
js3617 <a332210@naver.com>
2024-03-28 01:50:14 +0900

    Docs - App.js API 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import API from './hooks/NaverAPI';
 import GNB from './components/navbar/Gnb';
+
 
 function App() {
   return (
     <>
     <GNB/>
-    <API/>
     </>
   );
 }
