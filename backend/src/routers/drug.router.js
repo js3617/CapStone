f259d99d658f6b8de42b0b35e0b915d35ed49547
ryongseong <xmssnsk2119@gmail.com>
2024-05-07 11:24:00 +0900

    Modify - res.json에서 res.render 형식으로 변환

--- backend/src/routers/drug.router.js
+++ backend/src/routers/drug.router.js
@@ -1,38 +1,21 @@
-const express = require('express');
-const Drug = require('../models/drug.model');
-
-const router = express.Router();
+const 
+    express = require('express'),
+    Drug = require('../models/drug.model'),
+    router = express.Router(); 
 
 // drug 페이지로 간다면 모든 제품의 리스트를 보여줌
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
-        await Drug.find()
-                     .exec((err, drug) => {
-                        if(err) return res.status(400).send(err);
-                        return res.status(200).json({success: true, drug});
-                     })
-        
+        const drugs = await Drug.find();
+        res.render('drugs', { drugs });
     } catch (error) {
         console.error(error);
         next(error);
     }
 });
 
-router.get('/search_drug/:drug_id', async(req, res, next) => {
-    try {
-        const drug_id = req.params.drug_id;
-        const drug = await Drug.findById(drug_id);
-        res.json({
-            drug
-        })
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-})
-
 // 카테고리를 검색한다면 그 카테고리에 있는 값을 검색한다면
-router.get('/search_category/:category', async(req, res, next) => {
+router.get('/category/:category', async(req, res, next) => {
     // 검색한 값을 categorySlug 변수에 할당
     const categorySlug = req.params.category;
     try {
@@ -48,7 +31,7 @@ router.get('/search_category/:category', async(req, res, next) => {
 })
 
 // 이름을 검색한다면 
-router.get('/search_name/:name', async(req, res, next) => {
+router.get('/name/:name', async(req, res, next) => {
     // 검색한 값을 drugName 변수에 할당
     const drugName = req.params.name;
     try {
