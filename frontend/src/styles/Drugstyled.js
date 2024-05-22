c2b9ef322ea3de815d48de3de8a61d6a8e35bf9d
js3617 <a332210@naver.com>
2024-05-22 19:26:56 +0900

    Fix - styled-components 수정 및 추가

---
+++ frontend/src/styles/Drugstyled.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+const Text = styled.p`
+    font-size: 1.2rem;
+    display: flex;
+    justify-content: center;
+`
+const DetailText = styled.p`
+    font-size: 1.2rem;
+    display: flex;
+    text-align: left;
+    line-height: 2;
+`
+const DetailBText = styled.b`
+    font-size: 1.2rem;
+    display: flex;
+    text-align: left;
+`
+
+const DrugContainer = styled.div`
+    width: 100%;
+    display: flex;
+    margin-top: 8%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: calc(100vh - 180px);
+`
+const DrugDetailsWrapper = styled.div`
+  background-color: #FFFFFF;
+  padding: 40px;
+  border-radius: 30px;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%);
+  width: 1054px;
+  z-index: 10;
+`;
+
+const CloseButton = styled.button`
+  background-color:#FFFFFF;
+  color: #000000;
+  border: none;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 10;
+`;
+
+const DrugCard = styled.div`
+  background-color: #e8e8e8;
+  padding: 10px;
+  margin-bottom: 20px;
+  border-radius: 30px;
+  flex: 1;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const ImgCard = styled.div`
+  background-color: #FFFFFF;
+  padding: 10px;
+  border-radius: 20px;
+`;
+
+const DrugImage = styled.img`
+  width: 180px;
+  height: 120px;
+  margin: auto;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+export {DrugContainer, 
+    Text, 
+    DetailText, 
+    DetailBText, 
+    DrugDetailsWrapper, 
+    CloseButton,
+    DrugCard,
+    DrugImage,
+    ImgCard
+};
--- frontend/src/styles/styled.js
+++ frontend/src/styles/styled.js
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
-const Text = styled.p`
-    font-size: 1.2rem;
-    display: flex;
-    justify-content: center;
-`
-
 const Container = styled.div`
     width: 100%;
     height: 90vh;
@@ -13,19 +7,7 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    max-width: 1440px;
     min-height: calc(100vh - 180px);
 `
 
-const DrugContainer = styled.div`
-    width: 100%;
-    display: flex;
-    margin-top: 8%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    max-width: 1440px;
-    min-height: calc(100vh - 180px);
-`
-
-export {Container, DrugContainer, Text};
+export {Container};
