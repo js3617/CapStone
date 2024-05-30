df6a3011486966dd027f8074984c47d5bce41ff2
js3617 <a332210@naver.com>
2024-05-30 12:18:20 +0900

    Add - Store 페이지 구성

--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import API_store from "../hooks/NaverAPI_store";
 import StoreInformation from "../components/store/StoreInformation"
+import SearchComponents from "../components/search/Search";
+import LongBackground from '../components/background/LongBackgroundImage'
+import styled from 'styled-components';
+import { ListButton } from "../styles/styled";
+
+const StorePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 8%;
+`;
 
 function Store(){
+
     return(
-        <>
-        <API_store/>
-        <StoreInformation/>
-        </>
+        <StorePageContainer>
+            <LongBackground/>
+            <SearchComponents/>
+            <API_store/>
+            <StoreInformation/>
+            <Link to="/drug">
+                <ListButton>의약품 목록</ListButton>
+            </Link>
+        </StorePageContainer>
     )
 }
 
