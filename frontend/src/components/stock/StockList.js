3de117c63379c34ff31df4163f7f836fb74a983c
js3617 <a332210@naver.com>
2024-06-06 20:42:59 +0900

    Add - drugid 받아오기 기능 추가

--- frontend/src/components/stock/StockList.js
+++ frontend/src/components/stock/StockList.js
@@ -1,50 +1,61 @@
-// src/components/StoreInformation.js
 import React from 'react';
-import useStock from '../../hooks/useStock';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { LocationText, Name } from '../../styles/styled';
+import useStock from '../../hooks/useStock';
 
-const StoreContainer = styled.div`
-    display: flex;
-    width: 840px;
-    align-items: center;
-    background-color: #E8E8E8;
-    padding: 20px;
-    border-radius: 30px 30px 0 0;
-    z-index: 100;
-    position: relative;
-    top: -30px;
+const MapContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MapElement = styled.div`
+  width: 880px;
+  height: 460px;
 `;
 
 const StoreList = styled.ul`
-    list-style: none;
-    padding: 0;
-    width: 100%;
+  list-style: none;
+  padding: 0;
+  margin: 20px 0;
 `;
 
 const StoreItem = styled.li`
-    border-bottom: 1px solid #FFFFFF;
-    align-items: center;
-    &:last-child {
-        border-bottom: none; // 마지막 항목에는 구분선 없음
-    }
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 `;
 
 const StockList = () => {
-    const stores = useStock();
+  const { mapRef, id } = useParams();
+  const { stores, loading, error } = useStock(id);
 
-    return (
-        <StoreContainer>
-            <StoreList>
-                {stores.map((store, index) => (
-                    <StoreItem key={index}>
-                        <Name>{store.storeName}</Name>
-                        <LocationText>{store.storeAddr}</LocationText>
-                    </StoreItem>
-                ))}
-            </StoreList>
-        </StoreContainer>
-    );
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading stock data: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <MapContainer>
+        <MapElement id="map" ref={mapRef} />
+      </MapContainer>
+      <StoreList>
+        {stores && stores.map((store, index) => (
+          <StoreItem key={index}>
+            <div><strong>{store.storeName}</strong></div>
+            <div>{store.stock} items in stock</div>
+          </StoreItem>
+        ))}
+      </StoreList>
+    </div>
+  );
 };
 
 export default StockList;
