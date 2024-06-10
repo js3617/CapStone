177cab28717a8214261088932e5cc5a207a8f9ba
js3617 <a332210@naver.com>
2024-06-10 16:06:28 +0900

    Add - marker hover시 재고 내용 추가

--- frontend/src/hooks/NaverAPI_stock.js
+++ frontend/src/hooks/NaverAPI_stock.js
@@ -1,83 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import axios from 'axios';
-// import styled from 'styled-components';
-
-// const MapContainer = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const MapElement = styled.div`
-//   width: 880px;
-//   height: 460px;
-// `;
-
-// const { naver } = window;
-
-// const API_stock = () => {
-//   const { id } = useParams();
-//   console.log(id);
-//   const [markers, setMarkers] = useState([]);
-//   console.log(markers);
-//   const [map, setMap] = useState(null);
-//   console.log(map);
-
-//   useEffect(() => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition((position) => {
-//         const { latitude, longitude } = position.coords;
-//         initializeMap(latitude, longitude);
-//         fetchNearbyStoresWithStock(latitude, longitude, id);
-//       });
-//     } else {
-//       alert("Geolocation is not supported by this browser.");
-//     }
-//   }, [id]);
-
-//   const fetchNearbyStoresWithStock = async (latitude, longitude, drugID) => {
-//     try {
-//       const response = await axios.post(`http://localhost:3000/store/stock/${drugID}`, { latitude, longitude });
-//       setMarkers(response.data.stores);
-//     } catch (error) {
-//       console.error('Error fetching stores with stock:', error);
-//     }
-//   };
-
-//   const initializeMap = (latitude, longitude) => {
-//     const mapOptions = {
-//       center: new naver.maps.LatLng(latitude, longitude),
-//       zoom: 15,
-//     };
-//     const mapInstance = new naver.maps.Map('map', mapOptions);
-//     setMap(mapInstance);
-//   };
-
-//   useEffect(() => {
-//     if (map && markers.length > 0) {
-//       markers.forEach(marker => {
-//         new naver.maps.Marker({
-//           position: new naver.maps.LatLng(marker.location.coordinates[1], marker.location.coordinates[0]),
-//           map: map,
-//           title: marker.storeName,
-//         });
-//       });
-//     }
-//   }, [markers, map]);
-
-//   return (
-//     <MapContainer>
-//       <MapElement id="map" />
-//     </MapContainer>
-//   );
-// };
-
-// export default API_stock;
-
-// client/src/Map.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
@@ -109,7 +29,6 @@ const API_stock = () => {
         const { latitude, longitude } = position.coords;
         initializeMap(latitude, longitude);
         fetchNearbyStores(latitude, longitude, id);
-        console.log(latitude, longitude);
       });
     } else {
       alert("Geolocation is not supported by this browser.");
@@ -138,16 +57,38 @@ const API_stock = () => {
   useEffect(() => {
     if (map && markers.length > 0) {
       markers.forEach(marker => {
-        new naver.maps.Marker({
+        const markerInstance = new naver.maps.Marker({
           position: new naver.maps.LatLng(marker.location.coordinates[1], marker.location.coordinates[0]),
           map: map,
-          title: marker.storeName,
           icon: {
             url: '/images/redmarker.png',
-            size: new naver.maps.Size(22, 36),
+            size: new naver.maps.Size(42, 53),
             origin: new naver.maps.Point(0, 0),
             anchor: new naver.maps.Point(11, 35)
           }
+        });
+
+        // Create a custom overlay
+        const overlayContent = `
+          <div style="background-color: white; border: 1px solid #ccc; padding: 10px; border-radius: 5px;">
+            <strong>${marker.storeName}</strong><br />
+            재고: ${marker.stock}개
+          </div>
+        `;
+        const overlay = new naver.maps.InfoWindow({
+          content: overlayContent,
+          disableAnchor: true,
+          borderWidth: 0
+        });
+
+        // Show overlay on hover
+        naver.maps.Event.addListener(markerInstance, 'mouseover', () => {
+          overlay.open(map, markerInstance);
+        });
+
+        // Hide overlay when not hovering
+        naver.maps.Event.addListener(markerInstance, 'mouseout', () => {
+          overlay.close();
         });
       });
     }
@@ -157,7 +98,7 @@ const API_stock = () => {
     <MapContainer>
       <MapElement id="map" />
     </MapContainer>
-  )
+  );
 };
 
 export default API_stock;
