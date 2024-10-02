cd78bf2af0c9b488f44173de1116c14dc345d92b
js3617 <a332210@naver.com>
2024-10-02 16:14:20 +0900

    Fix - ClientID값 환경 변수로 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+// App.js
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Pharmacy from "./pages/Pharmacy";
+import Drug from "./pages/Drug";
+import Store from "./pages/Store";
+import Home from './pages/Home';
+import DrugSearch from './pages/Drugsearch';
+import Stock from './pages/Stock';
+import ScrollToTop from './components/scrolltop/ScrollTop';
 import GNB from './components/navbar/Gnb';
 
-
 function App() {
+  useEffect(() => {
+    console.log('ID:', process.env.REACT_APP_NCP_CLIENT_ID);
+    const script = document.createElement('script');
+    script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.REACT_APP_NCP_CLIENT_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+  }, []);
   return (
-    <>
-    <GNB/>
-    </>
+    <BrowserRouter>
+    <GNB />
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/drug' element={<Drug />} />
+        <Route path='/pharmacy' element={<Pharmacy />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/drug/search/:query' element={<DrugSearch />} />
+        <Route path="/store/stock/:id" element={<Stock />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
